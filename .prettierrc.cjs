9ac87080fbcb52d300e41b84203d8e645ccbd8b9
Rickard Natt och Dag <rickard@nattochdag.dev>
2023-03-07 18:40:33 +0100

    feat: add initial layout

---
+++ .prettierrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  plugins: [require.resolve('prettier-plugin-astro')],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+}
