d4b6b8a1b8e95be03c7bf2271f531a5a71d23d23
Rickard Natt och Dag <rdag@invidi.com>
2023-03-08 10:00:01 +0100

    chore: use serveless architecture

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config'
-import vercel from '@astrojs/vercel/static'
+import vercel from '@astrojs/vercel/serverless'
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   adapter: vercel({ analytics: true }),
   integrations: [
     tailwind({
